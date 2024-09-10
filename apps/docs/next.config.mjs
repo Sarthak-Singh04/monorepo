d35f44ce2a6cf0650463cd4862dfad92606bd50c
Sarthak-Singh04 <singhsarthak691@gmail.com>
2024-09-10 20:39:15 +0530

    remove the config change

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,9 +1,10 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  output: "standalone",
-  transpilePackages: ["ui"],
-  basePath: "/docs",
-  assetPrefix: "/docs",
-};
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+ if (process.env.NODE_ENV === 'development') {
+   await setupDevPlatform();
+ }
+
+export default nextConfig;
