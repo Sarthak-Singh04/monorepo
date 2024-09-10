481ab2d11f1e46fe4c053bb13ee7c026a870d547
Sarthak-Singh04 <singhsarthak691@gmail.com>
2024-09-10 20:25:20 +0530

    change the toml

---
+++ apps/docs/.vercel/output/static/_next/static/chunks/app/page-69f9c01f85829715.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{3208:function(n,e,u){Promise.resolve().then(u.t.bind(u,6829,23))}},function(n){n.O(0,[829,151,649,744],function(){return n(n.s=3208)}),_N_E=n.O()}]);
---
+++ apps/docs/.vercel/output/static/_next/static/lBqxOkAjcoireouO90nDu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-32e9a4978fc1f757.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
